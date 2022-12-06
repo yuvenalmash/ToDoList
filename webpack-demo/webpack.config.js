11a05cd6462f3dc5617e6fa41e7959285bb5840c
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-06 17:02:38 +0300

    Initial commit

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  
+};
