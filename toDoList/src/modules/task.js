bff6bf2c500dd774f298f25aaf593ff3e1578343
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-07 17:04:14 +0300

    list structure

---
+++ toDoList/src/modules/task.js
@@ -0,0 +1,7 @@
+export default class Task {
+  constructor(){
+    this.description = description;
+    this.completed = completed
+    this.index = index
+  }
+}
--- toDoList/webpack.config.js
+++ toDoList/webpack.config.js
@@ -2,21 +2,39 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+
   devServer: {
     static: './dist',
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
+
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     publicPath: '/',
   },
+
   optimization: {
     runtimeChunk: 'single',
   },
