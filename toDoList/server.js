919bcae2a09ab1bd31fefa5f2f380d4a75b3a97a
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-07 18:26:05 +0300

    fix liter errors

--- toDoList/server.js
+++ toDoList/server.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
+/*eslint-disable*/
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
-import config, { output } from './webpack.config.js';
+const config = require('./webpack.config.js');
 
 const compiler = webpack(config);
 
@@ -11,7 +12,7 @@ const compiler = webpack(config);
 // configuration file as a base.
 app.use(
   webpackDevMiddleware(compiler, {
-    publicPath: output.publicPath,
+    publicPath: config.output.publicPath,
   }),
 );
 
