c9d8e67be7f8a6fb26badf337496252b932cad25
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-08 11:55:05 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -59,23 +59,23 @@ import saveIcon from "./images/save1.png"
 // };
 
 
-// const add = () => {
-//   console.log("add clicked");
-//   const input = document.getElementById("inputToDo").value;
-//   if (input === ""){return}
-//   let toDoList = JSON.parse(localStorage.getItem("allEntries"));
-//   let index = null;
-//   if (toDoList == null) {
-//     index = 1;
-//   } else {
-//     index = toDoList.length + 1;
-//   }
-//   const newToDo = new ToDo(input, false, index);
-//   const storage = new TodoStorage();
-//   storage.addToDo(newToDo);
-//   document.getElementById("inputToDo").value = "";
-//   toDos();
-// };
+const add = () => {
+  console.log("add clicked");
+  const input = document.getElementById("inputToDo").value;
+  if (input === ""){return}
+  let toDoList = JSON.parse(localStorage.getItem("allEntries"));
+  let index = null;
+  if (toDoList == null) {
+    index = 1;
+  } else {
+    index = toDoList.length + 1;
+  }
+  const newToDo = new ToDo(input, false, index);
+  const storage = new TodoStorage();
+  storage.addToDo(newToDo);
+  document.getElementById("inputToDo").value = "";
+  toDos();
+};
 
 // const toDos = () => {
 //   clearToDos();
