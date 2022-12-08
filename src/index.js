e2a18490a1d6eb11cfce450912c001d99445045f
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-08 11:53:09 +0300

    import images

--- src/index.js
+++ src/index.js
@@ -1,44 +1,143 @@
-import './style.css';
-import menuIcon from './images/menu.png';
-import refreshIcon from './images/refresh2.png';
-import enterIcon from './images/enter.png';
+import "./style.css";
+import { toDos, addIcons } from "./modules/ui.js";
+import { add } from "./modules/add.js";
+import { edit } from "./modules/edit.js"
 
-const task1 = { description: 'task1', completed: false, index: 1 };
-const task2 = { description: 'task2', completed: false, index: 2 };
-const task3 = { description: 'task3', completed: false, index: 3 };
-const task4 = { description: 'task4', completed: false, index: 4 };
-const toDoList = [task1, task2, task3, task4];
 
-const addToHtml = (child, parentId) => {
-  const parent = document.getElementById(parentId);
-  parent.innerHTML += child;
-};
+import menuIcon from "./images/menu.png";
+import refreshIcon from "./images/refresh2.png";
+import enterIcon from "./images/enter.png";
+import binIcon from "./images/recycle-bin.png";
+import saveIcon from "./images/save1.png"
 
-const listToDo = (index) => {
-  const toDo = toDoList[index];
-  const menu = new Image();
-  menu.src = menuIcon;
-  const child = `
-    <div id=toDo> 
-    <div><input type="checkbox">${toDo.description}</div> 
-    <a href="#">${menu.outerHTML}</a>
-    </div>`;
-  addToHtml(child, 'toDos');
-};
+// const task1 = { description: 'task1', completed: false, index: 1 };
+// const task2 = { description: 'task2', completed: false, index: 2 };
+// const task3 = { description: 'task3', completed: false, index: 3 };
+// const task4 = { description: 'task4', completed: false, index: 4 };
+// const toDoList = [task1, task2, task3, task4];
 
-const addIcons = () => {
-  const refresh = new Image();
-  refresh.src = refreshIcon;
-  const enter = new Image();
-  enter.src = enterIcon;
-  const child = `<a href="#" class="icons">${refresh.outerHTML}</a>`;
-  const child2 = `<a href="#" class="icons">${enter.outerHTML}</a>`;
-  addToHtml(child, 'header');
-  addToHtml(child2, 'input');
-};
+// const addToHtml = (child, parentId) => {
+//   const parent = document.getElementById(parentId);
+//   parent.innerHTML += child;
+// };
 
-for (let i = 0; i < toDoList.length; i += 1) {
-  listToDo(i);
-  addToHtml('<hr>', 'toDos');
-}
+// const addIcons = () => {
+//   const refresh = new Image();
+//   refresh.src = refreshIcon;
+//   const enter = new Image();
+//   enter.src = enterIcon;
+//   const child = `<a id="refreshBtn" href="#" class="icons">${refresh.outerHTML}</a>`;
+//   const child2 = `<a id="addBtn" href="#" class="icons">${enter.outerHTML}</a>`;
+//   addToHtml(child, "header");
+//   addToHtml(child2, "input");
+// };
+
+// const clearToDos = () => {
+//   const child = "";
+//   const parent = document.getElementById("toDos");
+//   parent.innerHTML = child;
+// };
+
+// const listToDo = (toDoList, index) => {
+//   const toDo = toDoList[index];
+//   const menu = new Image();
+//   menu.src = menuIcon;
+//   const bin = new Image();
+//   bin.src = binIcon;
+//   const save = new Image();
+//   save.src = saveIcon;
+//   const child = `
+//     <div class="toDo" id="toDo${index}"> 
+//       <div>
+//         <input type="checkbox">
+//         <div>
+//           <p id="description${index}" style="display:block;" onclick="edit(${index})">${toDo.description}</p>
+//           <input  id="descriptionInput${index}" class="descriptionInput" type="text" style="display:none;border=none;" value="${toDo.description}">
+//         </div>
+//       </div> 
+//       <div>
+//         <a id="menuBtn${index}" style="display:block;" href="#">${menu.outerHTML}</a>
+//         <a id="saveBtn${index}" style="display:none;" onclick="save${index}" href="#">${save.outerHTML}</a>
+//         <a id="binBtn${index}" style="display:none;" onclick="remove(${index})" href="#">${bin.outerHTML}</a>
+//       </div>
+//     </div>`;
+//   addToHtml(child, "toDos");
+// };
+
+
+// const add = () => {
+//   console.log("add clicked");
+//   const input = document.getElementById("inputToDo").value;
+//   if (input === ""){return}
+//   let toDoList = JSON.parse(localStorage.getItem("allEntries"));
+//   let index = null;
+//   if (toDoList == null) {
+//     index = 1;
+//   } else {
+//     index = toDoList.length + 1;
+//   }
+//   const newToDo = new ToDo(input, false, index);
+//   const storage = new TodoStorage();
+//   storage.addToDo(newToDo);
+//   document.getElementById("inputToDo").value = "";
+//   toDos();
+// };
+
+// const toDos = () => {
+//   clearToDos();
+//   const toDoList = JSON.parse(localStorage.getItem("allEntries"));
+//   if (toDos == null) toDos = [];
+//   for (let i = 0; i < toDoList.length; i += 1) {
+//     listToDo(toDoList, i);
+//     addToHtml("<hr>", "toDos");
+//   }
+//   console.log(JSON.parse(localStorage.getItem("allEntries")))
+// };
+
+
+// const edit = (index) => {
+//   console.log("edit clicked")
+//   toDos();
+//   const menuIcon = document.getElementById(`menuBtn${index}`);
+//   menuIcon.style.display = "none";
+//   const binIcon = document.getElementById(`binBtn${index}`);
+//   binIcon.style.display = "block";
+//   const saveIcon = document.getElementById(`saveBtn${index}`);
+//   saveIcon.style.display = "block"
+//   const row = document.getElementById(`toDo${index}`)
+//   row.style.background = "rgb(233, 222, 128)"
+
+
+//   const p = document.getElementById(`description${index}`)
+//   const input = document.getElementById(`descriptionInput${index}`)
+//   p.style.display = "none"
+//   input.style.display = "block"
+// };
+window.edit = edit
+
+// const save = (index) => {
+//   const input = document.getElementById(`descriptionInput${index}`).value;
+//   if (input === ""){return}
+//   let toDoList = JSON.parse(localStorage.getItem("allEntries"));
+//   toDoList[index].description = input
+//   localStorage.setItem("allEntries", JSON.stringify(toDos));
+//   toDos()
+// }
+
+window.save = save
+
+
+// const remove = (index) => {
+//   console.log('remove clicked')
+//   const storage = new TodoStorage
+//   storage.removeToDo(index)
+//   toDos()
+// }
+window.remove = remove
+
+
 addIcons();
+toDos();
+document.querySelector("#addBtn").addEventListener("click", add);
+document.querySelector("#refreshBtn").addEventListener("click", toDos);
+console.log(JSON.parse(localStorage.getItem("allEntries")));
