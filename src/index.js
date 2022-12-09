0a9bbd1d67157000335c2659c9b63055c5f0c6ac
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-09 12:19:47 +0300

    use filter() to filter complete tasks in todo list

--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@ import './style.css';
 import { addIcons, toDos, add } from './modules/ui.js';
 
 const clearCompleted = () => {
-  const toDoList = JSON.parse(localStorage.getItem('allEntries'));
-  const complete = toDoList.filter((toDo) => toDo.completed === true);
-  const indexes = [];
-  for (let i = 0; i < complete.length; i += 1) {
-    const index = (complete[i].index) - 1;
-    indexes.push(index);
-  }
-
-  for (let i = indexes.length - 1; i >= 0; i -= 1) toDoList.splice(indexes[i], 1);
+  let toDoList = JSON.parse(localStorage.getItem('allEntries'));
+  toDoList = toDoList.filter((toDo) => toDo.completed === false);
 
   for (let i = 0; i < toDoList.length; i += 1) {
     toDoList[i].index = i + 1;
@@ -19,8 +12,7 @@ const clearCompleted = () => {
   toDos();
 };
 
-// const toDoList = null
-// localStorage.setItem("allEntries", JSON.stringify(toDoList));
+
 addIcons();
 toDos();
 document.querySelector('#addBtn').addEventListener('click', add);
