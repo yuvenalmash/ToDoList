706baa1ff2416ce29e6051d8f4fd721a2830c63a
Roy Jumah <royjumah17@gmail.com>
2022-12-14 20:55:00 +0300

    Fix es linting errors

--- tests/toDoStorage.test.js
+++ tests/toDoStorage.test.js
@@ -1,24 +1,24 @@
-import TodoStorage from "../src/modules/toDoStorage.js";
+import TodoStorage from '../src/modules/toDoStorage.js';
 
 const storage = new TodoStorage();
 
-describe("add todo ()", () => {
-  test("add todo", () => {
+describe('add todo ()', () => {
+  test('add todo', () => {
     const newToDo = {
-      descroption: "do this and that",
+      descroption: 'do this and that',
       completed: false,
       index: 0,
     };
     storage.addToDo(newToDo);
-    const toDoList = JSON.parse(localStorage.getItem("allEntries"));
+    const toDoList = JSON.parse(localStorage.getItem('allEntries'));
     expect(toDoList).toContainEqual(newToDo);
   });
 });
 
-describe("delete todo ()", () => {
-  test("remove todo", () => {
+describe('delete todo ()', () => {
+  test('remove todo', () => {
     storage.removeToDo(0);
-    const toDoList = JSON.parse(localStorage.getItem("allEntries"));
+    const toDoList = JSON.parse(localStorage.getItem('allEntries'));
     expect(toDoList).toHaveLength(0);
   });
 });
