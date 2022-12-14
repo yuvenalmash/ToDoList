759d3ff51a585e7690a8238139fa2972e2424900
Roy Jumah <royjumah17@gmail.com>
2022-12-14 20:51:56 +0300

    Wrap tests in describe methods

--- tests/toDoStorage.test.js
+++ tests/toDoStorage.test.js
@@ -1,20 +1,24 @@
-import TodoStorage from '../src/modules/toDoStorage.js';
+import TodoStorage from "../src/modules/toDoStorage.js";
 
 const storage = new TodoStorage();
 
-test('add todo', () => {
-  const newToDo = {
-    descroption: 'do this and that',
-    completed: false,
-    index: 0,
-  };
-  storage.addToDo(newToDo);
-  const toDoList = JSON.parse(localStorage.getItem('allEntries'));
-  expect(toDoList).toContainEqual(newToDo);
+describe("add todo ()", () => {
+  test("add todo", () => {
+    const newToDo = {
+      descroption: "do this and that",
+      completed: false,
+      index: 0,
+    };
+    storage.addToDo(newToDo);
+    const toDoList = JSON.parse(localStorage.getItem("allEntries"));
+    expect(toDoList).toContainEqual(newToDo);
+  });
 });
 
-test('remove todo', () => {
-  storage.removeToDo(0);
-  const toDoList = JSON.parse(localStorage.getItem('allEntries'));
-  expect(toDoList).toHaveLength(0);
+describe("delete todo ()", () => {
+  test("remove todo", () => {
+    storage.removeToDo(0);
+    const toDoList = JSON.parse(localStorage.getItem("allEntries"));
+    expect(toDoList).toHaveLength(0);
+  });
 });
