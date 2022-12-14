6839985cff52e366c41aedf6b955d6c39dc327f1
Roy Jumah <royjumah17@gmail.com>
2022-12-14 19:52:15 +0300

    Fix linting errors

--- tests/toDoStorage.test.js
+++ tests/toDoStorage.test.js
@@ -1,19 +1,20 @@
-import TodoStorage from "../src/modules/toDoStorage.js";
-const storage = new TodoStorage()
+import TodoStorage from '../src/modules/toDoStorage.js';
 
-test("add todo", () => {
+const storage = new TodoStorage();
+
+test('add todo', () => {
   const newToDo = {
-    descroption: "do this and that",
+    descroption: 'do this and that',
     completed: false,
     index: 0,
   };
-  storage.addToDo(newToDo)
-  const toDoList = JSON.parse(localStorage.getItem("allEntries"));
+  storage.addToDo(newToDo);
+  const toDoList = JSON.parse(localStorage.getItem('allEntries'));
   expect(toDoList).toContainEqual(newToDo);
 });
 
-test("remove todo", () => {
-  storage.removeToDo(0)
-  const toDoList = JSON.parse(localStorage.getItem("allEntries"));
-  expect(toDoList).toHaveLength(0)
-})
+test('remove todo', () => {
+  storage.removeToDo(0);
+  const toDoList = JSON.parse(localStorage.getItem('allEntries'));
+  expect(toDoList).toHaveLength(0);
+});
